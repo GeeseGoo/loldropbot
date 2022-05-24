b64261a0b18753f48edecf724fb891a5e79f937c
GeeseGoo <77707222+GeeseGoo@users.noreply.github.com>
2022-05-24 15:44:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,31 @@ service = Service(".\geckodriver.exe")
 service.start()
 driver = webdriver.Firefox()
 
-driver.get("https://lolesports.com/live")
-sleep(5)
+driver.get("https://lolesports.com")
+driver.maximize_window()
+sleep(3)
 driver.find_element(By.CSS_SELECTOR, "._2I66LI-wCuX47s2um7O7kh").click()
-sleep(5)
+sleep(3)
 action = webdriver.ActionChains(driver)
 action.send_keys(username, Keys.TAB, password, Keys.ENTER).perform()
-sleep(5)
+sleep(6)
+a = driver.find_elements(by=By.CLASS_NAME, value="live")
+for i in a:
+    print(i.get_attribute('href'))
 
 while True:
-    sleep(300)
-    if driver.current_url != "https://lolesports.com/live":
-        continue
-    driver.refresh()
+    driver.switch_to.window(driver.window_handles[0])
+    sleep(2)
+    streams = driver.find_elements(by=By.CLASS_NAME, value="live")
+    tabs = []
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        tabs.append(driver.current_url)
+        sleep(1)
+    for i in streams:
+        if i.get_attribute('href') not in tabs:
+            driver.execute_script(f"window.open('{i.get_attribute('href')}','_blank');")
+            driver.switch_to.window(driver.window_handles[-1])
+            sleep(5)
 
+    sleep(300)
